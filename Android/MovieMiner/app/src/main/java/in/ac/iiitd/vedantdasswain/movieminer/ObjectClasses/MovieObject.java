0630b313904fb2a7d1d14b7a7818d0c9274b0e17
vedantswain <vedant12118@iiitd.ac.in>
2015-04-01 19:58:20 +0530

    added new icon

---
+++ Android/MovieMiner/app/src/main/java/in/ac/iiitd/vedantdasswain/movieminer/ObjectClasses/MovieObject.java
@@ -0,0 +1,53 @@
+package in.ac.iiitd.vedantdasswain.movieminer.ObjectClasses;
+
+/**
+ * Created by vedantdasswain on 26/03/15.
+ */
+public class MovieObject {
+    String fb_id,imdb_id,title,director,actors,genre,imageUri;
+
+    public MovieObject(String fb_id, String imdb_id, String title,
+                    String director, String actors,String genre, String imageUri){
+        this.fb_id=fb_id;
+        this.imdb_id=imdb_id;
+        this.title=title;
+        this.director=director;
+        this.actors=actors;
+        this.genre=genre;
+        this.imageUri=imageUri;
+    }
+
+    public String getFb_id(){
+        return this.fb_id;
+    }
+
+
+    public String getImdb_id(){
+        return this.imdb_id;
+    }
+
+
+    public String getTitle(){
+        return this.title;
+    }
+
+
+    public String getDirector(){
+        return this.director;
+    }
+
+
+    public String getActors(){
+        return this.actors;
+    }
+
+
+    public String getGenre(){
+        return this.genre;
+    }
+
+    public String getImageUri(){
+        return this.imageUri;
+    }
+
+}
--- Django/MovieMiner/y/google-cloud-sdk/bin/GoogleAppEngineLauncher.app
+++
@@ -1,1 +0,0 @@
-../platform/GoogleAppEngineLauncher.app
